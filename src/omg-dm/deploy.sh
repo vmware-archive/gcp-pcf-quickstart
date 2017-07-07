fbbe4635ff812dfc27f69b5bd11806fc67d78cfc
Jeff Johnson <jrjohnson@google.com>
2017-07-07 12:48:56 -0700

    ert/http_routing: add https

--- src/omg-dm/deploy.sh
+++ src/omg-dm/deploy.sh
@@ -4,7 +4,18 @@ set -uex
 envName=$(date +%s)
 projectOwner=$(gcloud config get-value account)
 
-echo "export envName=${envName}" >> .envrc
+mkdir -p ssl
+pushd ssl
+  openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+  openssl rsa -passin pass:x -in server.pass.key -out server.key
+  openssl req -new -key server.key -out server.csr \
+  -subj "/C=US/ST=Washington/L=Seattle/CN=*.omg.cloudnativeapp.com"
+  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+popd
+
+echo "
+export envName=${envName}
+" >> .envrc
 direnv allow || true
 
-gcloud deployment-manager deployments create omg-${envName} --config=omg.jinja --properties=projectOwner:${projectOwner}
+gcloud deployment-manager deployments create omg-${envName} --config=omg.jinja --properties="projectOwner:${projectOwner}"
--- src/omg-dm/update.sh
+++ src/omg-dm/update.sh
@@ -3,4 +3,4 @@ set -uex
 
 projectOwner=$(gcloud config get-value account)
 
-gcloud deployment-manager deployments update omg-${envName} --config=omg.jinja --properties=projectOwner:${projectOwner}
+gcloud deployment-manager deployments update omg-${envName} --config=omg.jinja --properties="projectOwner:${projectOwner}"
