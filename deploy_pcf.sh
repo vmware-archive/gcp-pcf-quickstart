vmware-archive/gcp-pcf-quickstart

commit d7157ed45e8ae2917cca65284e9b922e8c6e69c8

all: Deploy GCP Service Broker
- provision CloudSQL db for storage
- configure/deploy tile as part of `omg-cli deploy`

requires the stemcell to be uploaded out of band. this will be addressed
with a new base image.

deploy_pcf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -ue
+cd "$(dirname "$0")"
 
 if [ -z ${DNS_ZONE_NAME+x} ]; then
     echo "DNS_ZONE_NAME required"
@@ -31,7 +32,5 @@ pushd src/omg-tf
 popd
 
 # Deploy PCF
-pushd src/omg-cli
-    go build
-    ./omg-cli bootstrap-deploy --ssh-key-path ../omg-tf/keys/jumpbox_ssh --username omg --terraform-output-path ${terraform_output}
-popd
+go install omg-cli
+omg-cli bootstrap-deploy --ssh-key-path src/omg-tf/keys/jumpbox_ssh --username omg --terraform-output-path ${terraform_output}

src/omg-cli/config/config.go
@@ -27,8 +27,12 @@ type Config struct {
 	SslCertificate                     string
 	SslPrivateKey                      string
 	OpsManServiceAccount               string
-	ServiceBrokerServiceAccountKey     string
 	StackdriverNozzleServiceAccountKey string
+
+	ServiceBrokerServiceAccountKey string
+	ServiceBrokerDbIp              string
+	ServiceBrokerDbUsername        string
+	ServiceBrokerDbPassword        string
 
 	Region      string
 	Zone1       string

src/omg-cli/config/terraform_config.go
@@ -33,8 +33,12 @@ type TerraformConfigSchema struct {
 	SslCertificate                     string `json:"ssl_cert"`
 	SslPrivateKey                      string `json:"ssl_cert_private_key"`
 	OpsManServiceAccount               string `json:"service_account_email"`
-	ServiceBrokerServiceAccountKey     string `json:"servicebroker_service_account_key"`
 	StackdriverNozzleServiceAccountKey string `json:"stackdriver_service_account_key"`
+
+	ServiceBrokerServiceAccountKey string `json:"service_broker_service_account_key"`
+	ServiceBrokerDbIp              string `json:"service_broker_db_ip"`
+	ServiceBrokerDbUsername        string `json:"service_broker_db_username"`
+	ServiceBrokerDbPassword        string `json:"service_broker_db_password"`
 
 	Region      string `json:"region"`
 	Zone1       string `json:"azs_0"`

src/omg-cli/omg/commands/commands.go
@@ -25,8 +25,8 @@ const (
 var selectedTiles = []tiles.TileInstaller{
 	gcp_director.Tile{},
 	ert.Tile{},
-	stackdriver_nozzle.Tile{},
 	service_broker.Tile{},
+	stackdriver_nozzle.Tile{},
 }
 
 type register interface {

src/omg-cli/omg/setup/ops_manager.go
@@ -123,6 +123,7 @@ func (s *OpsManager) PoolTillOnline() error {
 
 func (s *OpsManager) ConfigureTiles() error {
 	for _, t := range s.tiles {
+		s.logger.Printf("configuring tile: %s", t.Definition().Product.Name)
 		if err := t.Configure(s.cfg, s.om); err != nil {
 			return err
 		}

src/omg-cli/omg/tiles/configuration.go
@@ -1,5 +1,9 @@
 package tiles
 
+import (
+	"omg-cli/config"
+)
+
 type AvalibilityZone struct {
 	Name string `json:"name"`
 }
@@ -12,4 +16,12 @@ type Network struct {
 	SingletonAvalibilityZone AvalibilityZone   `json:"singleton_availability_zone"`
 	OtherAvailabilityZones   []AvalibilityZone `json:"other_availability_zones"`
 	Network                  NetworkName       `json:"network"`
+}
+
+func NetworkConfig(subnetName string, cfg *config.Config) Network {
+	return Network{
+		AvalibilityZone{cfg.Zone1},
+		[]AvalibilityZone{{cfg.Zone1}, {cfg.Zone2}, {cfg.Zone3}},
+		NetworkName{subnetName},
+	}
 }

src/omg-cli/omg/tiles/ert/configuration.go
@@ -4,22 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"omg-cli/config"
+	"omg-cli/omg/tiles"
 	"omg-cli/ops_manager"
 )
-
-type AvalibilityZone struct {
-	Name string `json:"name"`
-}
-
-type NetworkName struct {
-	Name string `json:"name"`
-}
-
-type Network struct {
-	SingletonAvalibilityZone AvalibilityZone   `json:"singleton_availability_zone"`
-	OtherAvailabilityZones   []AvalibilityZone `json:"other_availability_zones"`
-	Network                  NetworkName       `json:"network"`
-}
 
 type Properties struct {
 	// Domains
@@ -104,11 +91,7 @@ func (Tile) Configure(cfg *config.Config, om *ops_manager.Sdk) error {
 		return err
 	}
 
-	network := Network{
-		AvalibilityZone{cfg.Zone1},
-		[]AvalibilityZone{{cfg.Zone1}, {cfg.Zone2}, {cfg.Zone3}},
-		NetworkName{cfg.ErtSubnetName},
-	}
+	network := tiles.NetworkConfig(cfg.ErtSubnetName, cfg)
 
 	networkBytes, err := json.Marshal(&network)
 	if err != nil {

src/omg-cli/omg/tiles/gcp_director/resource.go
@@ -4,11 +4,16 @@ import (
 	"omg-cli/config"
 )
 
+var tile = config.Tile{
+	Product: config.OpsManagerMetadata{
+		Name: "BOSH Director",
+	},
+}
+
 type Tile struct{}
 
 func (Tile) Definition() config.Tile {
-	panic("not applicable to built in tiles")
-	return config.Tile{}
+	return tile
 }
 func (Tile) BuiltIn() bool {
 	return true

commit 31bb26b3e2899b654130d92eb4ba3f8d6978b9cb

update base image to include stemcell
Required by service broker introduced in d7157ed4

deploy_pcf.sh
@@ -16,7 +16,7 @@ fi
 gcloud config set project ${PROJECT_ID}
 
 if [ -z ${BASE_IMAGE_SELFLINK+x} ]; then
-    export BASE_IMAGE_SELFLINK="projects/graphite-demo-jjcf/global/images/baked-opsman-1501021113"
+    export BASE_IMAGE_SELFLINK="projects/jrjohnsonkube/global/images/baked-opsman-1501261888"
     echo "BASE_IMAGE_SELFLINK unset, using: ${BASE_IMAGE_SELFLINK}"
 fi
 