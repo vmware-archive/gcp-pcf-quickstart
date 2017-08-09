229cdf6bf1290ff622d520447db49f6fbac5a5f5
Jeff Johnson <jrjohnson@google.com>
2017-08-08 17:46:49 -0700

    helper: add script to publish image to GCS

---
+++ util/publish_image.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+if [ -z ${BASE_IMAGE_SELFLINK+x} ]; then
+    echo "BASE_IMAGE_SELFLINK required"
+    exit 1
+fi
+
+if [ -z ${DESTINATION_BUCKET+x} ]; then
+    echo "DESTINATION_BUCKET required"
+    exit 1
+fi
+
+if [ -z ${DESTINATION_NAME+x} ]; then
+    echo "DESTINATION_NAME required"
+    exit 1
+fi
+
+instance=baked-opsman
+iamge_snapshot=image-snapshot
+zone=us-east1-c
+
+gcloud compute instances create --image ${BASE_IMAGE_SELFLINK} ${instance} --zone=${zone} --machine-type=n1-standard-32
+gcloud compute instances stop ${instance} --zone=${zone}
+gcloud compute disks snapshot ${instance} --snapshot-names ${iamge_snapshot} --zone=${zone}
+gcloud compute disks create image-disk --source-snapshot ${iamge_snapshot} --zone=${zone} --type=pd-ssd
+
+startup=$(mktemp)
+cat << STARTUP_SCRIPT > ${startup}
+sudo mkdir /mnt/tmp
+sudo mkfs.ext4 -F /dev/disk/by-id/google-local-ssd-0
+sudo mount -o discard,defaults /dev/disk/by-id/google-local-ssd-0 /mnt/tmp
+sudo dd if=/dev/disk/by-id/google-image-disk of=/mnt/tmp/disk.raw bs=4096
+cd /mnt/tmp
+sudo tar czvf myimage.tar.gz disk.raw
+gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp /mnt/tmp/myimage.tar.gz gs://${DESTINATION_BUCKET}/${DESTINATION_NAME}.tar.gz
+shutdown -h now 'done'
+STARTUP_SCRIPT
+
+gcloud compute instances create baked-opsman-capture --scopes storage-rw \
+    --disk name=image-disk,device-name=image-disk \
+    --image-family ubuntu-1604-lts \
+    --image-project ubuntu-os-cloud \
+    --local-ssd interface=scsi \
+    --zone=${zone} \
+    --machine-type=n1-standard-32 \
+    --metadata-from-file startup-script=${startup}
+
+gcloud compute instances tail-serial-port-output baked-opsman-capture --zone=${zone}
