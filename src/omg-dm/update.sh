b90adfd497e24b00ce22c417129f4a129e1e332e
Jeff Johnson <jrjohnson@google.com>
2017-08-08 17:54:15 -0700

    remove unused deployment manager code

--- src/omg-dm/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -uex
-
-projectOwner=$(gcloud config get-value account)
-envName=${envName}
-rootDomain=${rootDomain}
-
-gcloud deployment-manager deployments update omg-${envName} --config=omg.jinja --properties="projectOwner:${projectOwner},rootDomain:${rootDomain},region:${region},zone1:${zone1},zone2:${zone2},zone3:${zone3}"
