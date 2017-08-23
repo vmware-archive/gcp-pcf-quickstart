7cce7a3b341f8b6607f2a197222d97c20ad60bd1
Jeff Johnson <jrjohnson@google.com>
2017-08-22 22:27:37 -0700

    ci: generate-env: correct env directory

--- ci/tasks/generate-env.sh
+++ ci/tasks/generate-env.sh
@@ -5,10 +5,11 @@ set -e
 
 my_dir="$( cd $(dirname $0) && pwd )"
 release_dir="$( cd ${my_dir} && cd ../.. && pwd )"
-workspace_dir="${release_dir}"
+workspace_dir="$( cd ${release_dir} && cd .. && pwd )"
 omg_tf_dir="${release_dir}/src/omg-tf"
+env_output_dir="${workspace_dir}/omg-env-out"
 
-export GOPATH=${workspace_dir}
+export GOPATH=${release_dir}
 export PATH=${GOPATH}/bin:${PATH}
 
 pushd ${release_dir} > /dev/null
@@ -18,10 +19,10 @@ popd > /dev/null
 check_param ${google_region}
 check_param ${env_file_name}
 check_param ${env_name}
-check_param ${IVNET_API_TOKEN}
-check_param ${IVNET_ACCEPT_EULA}
-check_param ${NS_ZONE_NAME}
-check_param ${ASE_IMAGE_URL}
+check_param ${PIVNET_API_TOKEN}
+check_param ${PIVNET_ACCEPT_EULA}
+check_param ${DNS_ZONE_NAME}
+check_param ${BASE_IMAGE_URL}
 
 set_gcloud_config
 
@@ -34,7 +35,7 @@ pushd ${omg_tf_dir}
 	./init.sh
 popd
 
-env_file="${workspace_dir}/omg-env-out/${env_file_name}"
+env_file="${env_output_dir}/${env_file_name}"
 pushd "${ENV_DIR}"
 	tar czvf ${env_file} .
 popd
