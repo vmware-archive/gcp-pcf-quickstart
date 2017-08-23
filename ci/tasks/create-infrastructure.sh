c19b44f96bd884434e4afbcd95727b5a72005f49
Jeff Johnson <jrjohnson@google.com>
2017-08-22 23:52:19 -0700

    ci: go to proper dir

--- ci/tasks/create-infrastructure.sh
+++ ci/tasks/create-infrastructure.sh
@@ -6,7 +6,7 @@ set -e
 my_dir="$( cd $(dirname $0) && pwd )"
 release_dir="$( cd ${my_dir} && cd ../.. && pwd )"
 workspace_dir="$( cd ${release_dir} && cd .. && pwd )"
-omg_dir="${release_dir}/src/omg-cli"
+omg_tf_dir="${release_dir}/src/omg-tf"
 env_dir="${workspace_dir}/env"
 env_file="${workspace_dir}/omg-env-in/${env_file_name}"
 env_output_dir="${workspace_dir}/omg-env-out"
@@ -28,10 +28,12 @@ terraform_output="${env_dir}/env.json"
 terraform_config="${env_dir}/terraform.tfvars"
 terraform_state="${env_dir}/terraform.tfstate"
 
-terraform init
-terraform get
-terraform apply --parallelism=100 -state=${terraform_state} -var-file=${terraform_config} || terraform apply --parallelism=100 -state=${terraform_state} -var-file=${terraform_config}
-terraform output -json -state=${terraform_state} > ${terraform_output}
+pushd "${omg_tf_dir}"
+	terraform init
+	terraform get
+	terraform apply --parallelism=100 -state=${terraform_state} -var-file=${terraform_config} || terraform apply --parallelism=100 -state=${terraform_state} -var-file=${terraform_config}
+	terraform output -json -state=${terraform_state} > ${terraform_output}
+popd
 
 env_file="${env_output_dir}/${env_file_name}"
 pushd "${env_dir}"
