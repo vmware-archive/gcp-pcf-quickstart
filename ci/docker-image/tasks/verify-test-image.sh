2a47c31685b99199545efaeb17ce33efdb65f252
Jeff Johnson <jrjohnson@google.com>
2017-08-22 22:33:55 -0700

    ci: docker-image: grab gomega/ginkgo before running tests

--- ci/docker-image/tasks/verify-test-image.sh
+++ ci/docker-image/tasks/verify-test-image.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-
 my_dir="$( cd $(dirname $0) && pwd )"
 release_dir="$( cd ${my_dir} && cd ../../.. && pwd )"
 workspace_dir="${release_dir}"
 test_dir="${release_dir}/ci/docker-image/system_tests"
 
 pushd ${test_dir} > /dev/null
+	go get github.com/onsi/gomega
+	go get github.com/onsi/ginkgo
 	ginkgo -r .
 popd > /dev/null
