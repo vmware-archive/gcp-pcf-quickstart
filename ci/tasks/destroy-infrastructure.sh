cd402962db5ede8a9dbdc7b02ed62273871e6170
Jeff Johnson <jrjohnson@google.com>
2017-08-23 19:35:03 -0700

    ci: standup destroy-infrastructure, queue push-tiles

---
+++ ci/tasks/destroy-infrastructure.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+my_dir="$( cd $(dirname $0) && pwd )"
+release_dir="$( cd ${my_dir} && cd ../.. && pwd )"
+workspace_dir="$( cd ${release_dir} && cd .. && pwd )"
+omg_tf_dir="${release_dir}/src/omg-tf"
+env_dir="${workspace_dir}/env"
+env_file="${workspace_dir}/omg-env-in/${env_file_name}"
+env_output_dir="${workspace_dir}/omg-env-out"
+
+pushd ${release_dir} > /dev/null
+	source ci/tasks/utils.sh
+popd > /dev/null
+check_param ${env_file_name}
+
+mkdir -p ${env_dir}
+pushd ${env_dir}
+	tar zxvf ${env_file}
+popd
+
+export GOPATH=${release_dir}
+export PATH=${GOPATH}/bin:${PATH}
+
+terraform_output="${env_dir}/env.json"
+terraform_config="${env_dir}/terraform.tfvars"
+terraform_state="${env_dir}/terraform.tfstate"
+
+pushd "${omg_tf_dir}"
+	yes "yes" | terraform destroy --parallelism=100 -state=${terraform_state} -var-file=${terraform_config}
+popd
+
+env_file="${env_output_dir}/${env_file_name}"
+pushd "${env_dir}"
+	tar czvf ${env_file} .
+popd
