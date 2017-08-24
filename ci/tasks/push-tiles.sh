d56a42248b1cfbd71dc07bfd717999c2226503a6
Jeff Johnson <jrjohnson@google.com>
2017-08-23 19:58:25 -0700

    ci: push-tiles: correct workspace

--- ci/tasks/push-tiles.sh
+++ ci/tasks/push-tiles.sh
@@ -5,7 +5,7 @@ set -e
 
 my_dir="$( cd $(dirname $0) && pwd )"
 release_dir="$( cd ${my_dir} && cd ../.. && pwd )"
-workspace_dir="${release_dir}"
+workspace_dir="$( cd ${release_dir} && cd .. && pwd )"
 omg_dir="${release_dir}/src/omg-cli"
 env_file="${workspace_dir}/omg-env/${env_file_name}"
 env_dir="${workspace_dir}/env"
@@ -20,7 +20,7 @@ pushd ${env_dir}
 	tar zxvf ${env_file}
 popd
 
-export GOPATH=${workspace_dir}
+export GOPATH=${release_dir}
 export PATH=${GOPATH}/bin:${PATH}
 
 go install omg-cli
