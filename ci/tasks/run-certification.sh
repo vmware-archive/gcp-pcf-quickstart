b8d80602caf66818667cf176fdb91cd65a4546ce
Jeff Johnson <jrjohnson@google.com>
2017-10-17 14:17:14 -0700

    ci: run certification tests as part of pipeline

---
+++ ci/tasks/run-certification.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+my_dir="$( cd $(dirname $0) && pwd )"
+release_dir="$( cd ${my_dir} && cd ../.. && pwd )"
+workspace_dir="${release_dir}"
+omg_dir="${release_dir}/src/omg-cli"
+env_file="${workspace_dir}/omg-env/${env_file_name}"
+env_dir="${workspace_dir}/env"
+
+pushd ${release_dir} > /dev/null
+	source ci/tasks/utils.sh
+popd > /dev/null
+check_param ${env_file_name}
+
+mkdir -p ${env_dir}
+pushd ${env_dir}
+	tar zxvf ${env_file}
+popd
+
+export GOPATH=${workspace_dir}
+export PATH=${GOPATH}/bin:${PATH}
+export ENV_DIR=${env_dir}
+
+pushd ${omg_dir}
+	ginkgo -r certification
+popd
