0187a643e78f185fa888ffc9995b1f89dbb59d53
Jeff Johnson <jrjohnson@google.com>
2017-11-30 10:34:01 -0800

    omg-cli/config/env_config: update opsman image
    This version makes stemcell updates easier
    
    Change-Id: I2345f568914ebb3b04e3f244d67c5a082bd2fed6

--- src/omg-cli/config/env_config.go
+++ src/omg-cli/config/env_config.go
@@ -38,7 +38,7 @@ type EnvConfig struct {
 func DefaultEnvConfig() (*EnvConfig, error) {
 	c := &EnvConfig{
 		DnsZoneName:  "pcf-zone",
-		BaseImageURL: "https://storage.cloud.google.com/ops-manager-us/pcf-gcp-1.12.2.tar.gz",
+		BaseImageURL: "https://storage.cloud.google.com/ops-manager-us/pcf-gcp-1.12.5.tar.gz",
 		EnvName:      "pcf",
 		Region:       "us-east1",
 		Zone1:        "us-east1-b",
