d2963602abcdbe9b312680298035743844303ea9
Jeff Johnson <jrjohnson@google.com>
2017-12-04 20:11:36 -0800

    ci/tasks/destroy-pcf: add retry on failure
    when the system is failing, it's likely the delete-installation will
    fail. add a retry with delay (to wait on BOSH locks) so we don't go
    straight to the terraform step which will fail due to existing
    instances.

--- ci/tasks/destroy-pcf.sh
+++ ci/tasks/destroy-pcf.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-
-
 my_dir="$( cd $(dirname $0) && pwd )"
 pushd ${my_dir} > /dev/null
 	source utils.sh
@@ -11,4 +9,14 @@ pushd ${my_dir} > /dev/null
 popd > /dev/null
 
 go install omg-cli
-omg-cli remote --env-dir="${env_dir}" "delete-installation"
+
+set +e
+n=0
+until [ $n -ge 5 ]
+do
+  omg-cli remote --env-dir="${env_dir}" "delete-installation" && break
+  n=$[$n+1]
+  delay=180
+  echo "delete failed, trying again in ${delay} seconds"
+  sleep ${delay}
+done
