06716625bc27fe20fe16b265d1af561259f4a3c3
Jeff Johnson <jrjohnson@google.com>
2017-12-06 09:57:32 -0800

    omg-cli/google/quotas: move package declaration
    it should be under the copyright
    
    Change-Id: I78c74a197f89bfd13ce4b672483925504eadd020

--- src/omg-cli/google/quotas.go
+++ src/omg-cli/google/quotas.go
@@ -1,5 +1,3 @@
-package google
-
 /*
  * Copyright 2017 Google Inc.
  *
@@ -15,6 +13,8 @@ package google
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package google
 
 import (
 	"context"
