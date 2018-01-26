497fb67f4cbd82fd4ae48a91c947f75cf680432e
Jeff Johnson <jrjohnson@google.com>
2018-01-25 18:25:47 -0800

    omg-tf: use cloud sql gen 1 for faster creation time
    These instances spin up in less than a minute vs. 5
    
    Change-Id: Idfe1d3315a734f01577b79c4e7f7a672446763d0

--- src/omg-tf/modules.tf
+++ src/omg-tf/modules.tf
@@ -5,7 +5,7 @@ module "external_database" {
 
   env_name    = "${var.env_name}"
   region      = "${var.region}"
-  sql_db_tier = "db-f1-micro"
+  sql_db_tier = "D0"
 
   ert_sql_db_host    = "${var.ert_sql_db_host}"
   opsman_sql_db_host = "${var.opsman_sql_db_host}"
