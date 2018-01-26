vmware-archive/gcp-pcf-quickstart

commit 03f92b336f11b7f6fd8fe9fbab429668e5d6884a

Add Small Footprint Runtime
- Add support for Samll Footprint Runtime tile
- Shrink terraform deployed resources:
  - n1-standard-1 jumpbox always
  - 1x NAT for small deployments
- Default to small deployments, configurable by 'SmallFootprint' flag in
  the env config (env/pcf/config.json).

This new deployment is not for production workloads. It results in a
full featured PCF but limited in scale. It should significantly speed up
deployment time (by ~2hrs) and make the default deployment fit in a
default quota.

src/omg-cli/omg/tiles/ert/configuration.go
@@ -82,7 +82,7 @@ type Properties struct {
 	MySqlMonitorRecipientEmail tiles.Value `json:".mysql_monitor.recipient_email"`
 }
 
-type Resources struct {
+type LargeFootprintResources struct {
 	TcpRouter                    tiles.Resource `json:"tcp_router"`
 	Router                       tiles.Resource `json:"router"`
 	DiegoBrain                   tiles.Resource `json:"diego_brain"`
@@ -117,8 +117,33 @@ type Resources struct {
 	MysqlRejoinUnsafe            tiles.Resource `json:"mysql-rejoin-unsafe"`
 }
 
-func (*Tile) Configure(cfg *config.Config, om *ops_manager.Sdk) error {
-	if err := om.StageProduct(tile.Product); err != nil {
+type SmallFootprintResources struct {
+	TcpRouter tiles.Resource `json:"tcp_router"`
+	Router    tiles.Resource `json:"router"`
+
+	Database    tiles.Resource `json:"database"`
+	Control     tiles.Resource `json:"control"`
+	Compute     tiles.Resource `json:"compute"`
+	FileStorage tiles.Resource `json:"blobstore"`
+
+	HaProxy                   tiles.Resource `json:"ha_proxy"`
+	BackupPrepare             tiles.Resource `json:"backup-prepare"`
+	MysqlMonitor              tiles.Resource `json:"mysql_monitor"`
+	SmokeTests                tiles.Resource `json:"smoke-tests"`
+	PushAppsManager           tiles.Resource `json:"push-apps-manager"`
+	Notifications             tiles.Resource `json:"notifications"`
+	NotificationsUi           tiles.Resource `json:"notifications-ui"`
+	PushPivotalAccount        tiles.Resource `json:"push-pivotal-account"`
+	PushUsageService          tiles.Resource `json:"push-usage-service"`
+	Autoscaling               tiles.Resource `json:"autoscaling"`
+	AutoscalingRegisterBroker tiles.Resource `json:"autoscaling-register-broker"`
+	Nfsbrokerpush             tiles.Resource `json:"nfsbrokerpush"`
+	Bootstrap                 tiles.Resource `json:"bootstrap"`
+	MysqlRejoinUnsafe         tiles.Resource `json:"mysql-rejoin-unsafe"`
+}
+
+func (*Tile) Configure(envConfig *config.EnvConfig, cfg *config.Config, om *ops_manager.Sdk) error {
+	if err := om.StageProduct(product); err != nil {
 		return err
 	}
 
@@ -184,28 +209,58 @@ func (*Tile) Configure(cfg *config.Config, om *ops_manager.Sdk) error {
 		return err
 	}
 
-	resoruces := Resources{
-		TcpRouter: tiles.Resource{
-			RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.TcpTargetPoolName)},
-			InternetConnected: false,
-			Instances:         3,
-		},
-		Router: tiles.Resource{
-			RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.WssTargetPoolName), fmt.Sprintf("http:%s", cfg.HttpBackendServiceName)},
-			InternetConnected: false,
-		},
-		DiegoBrain: tiles.Resource{
-			RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.SshTargetPoolName)},
-			InternetConnected: false,
-		},
-		HaProxy: tiles.Resource{
-			Instances: 0,
-		},
+	resorucesBytes := []byte{}
+
+	if envConfig.SmallFootprint {
+		resoruces := SmallFootprintResources{
+			TcpRouter: tiles.Resource{
+				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.TcpTargetPoolName)},
+				InternetConnected: false,
+				Instances:         1,
+			},
+			Router: tiles.Resource{
+				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.WssTargetPoolName), fmt.Sprintf("http:%s", cfg.HttpBackendServiceName)},
+				InternetConnected: false,
+			},
+			Control: tiles.Resource{
+				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.SshTargetPoolName)},
+				InternetConnected: false,
+			},
+			HaProxy: tiles.Resource{
+				Instances: 0,
+			},
+			MysqlMonitor: tiles.Resource{
+				Instances: 0,
+			},
+		}
+		resorucesBytes, err = json.Marshal(&resoruces)
+	} else {
+		resoruces := LargeFootprintResources{
+			TcpRouter: tiles.Resource{
+				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.TcpTargetPoolName)},
+				InternetConnected: false,
+				Instances:         1,
+			},
+			Router: tiles.Resource{
+				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.WssTargetPoolName), fmt.Sprintf("http:%s", cfg.HttpBackendServiceName)},
+				InternetConnected: false,
+			},
+			DiegoBrain: tiles.Resource{
+				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.SshTargetPoolName)},
+				InternetConnected: false,
+			},
+			HaProxy: tiles.Resource{
+				Instances: 0,
+			},
+			MysqlMonitor: tiles.Resource{
+				Instances: 0,
+			},
+		}
+		resorucesBytes, err = json.Marshal(&resoruces)
 	}
-	resorucesBytes, err := json.Marshal(&resoruces)
+
 	if err != nil {
 		return err
 	}
-
-	return om.ConfigureProduct(tile.Product.Name, string(networkBytes), string(propertiesBytes), string(resorucesBytes))
+	return om.ConfigureProduct(product.Name, string(networkBytes), string(propertiesBytes), string(resorucesBytes))
 }

src/omg-cli/omg/tiles/configuration.go
@@ -75,4 +75,5 @@ type Resource struct {
 	RouterNames       []string `json:"elb_names,omitempty"`
 	Instances         int      `json:"instances,omitempty"`
 	InternetConnected bool     `json:"internet_connected"`
+	VmTypeId          string   `json:"vm_type_id,omitempty"`
 }

src/omg-cli/omg/tiles/gcp_director/configuration.go
@@ -31,10 +31,10 @@ const (
 	metadataService = "169.254.169.254"
 )
 
-func (*Tile) Configure(cfg *config.Config, om *ops_manager.Sdk) error {
+func (*Tile) Configure(envConfig *config.EnvConfig, cfg *config.Config, om *ops_manager.Sdk) error {
 	networks, networkAssignment := networkCfg(cfg)
 
-	return om.SetupBosh(gcp(cfg), director(cfg), avalibilityZones(cfg), networks, networkAssignment, resources())
+	return om.SetupBosh(gcp(cfg), director(cfg), avalibilityZones(cfg), networks, networkAssignment, resources(envConfig))
 }
 
 func buildNetwork(cfg *config.Config, name, cidrRange, gateway string, serviceNetwork bool) commands.NetworkConfiguration {
@@ -102,8 +102,9 @@ func director(cfg *config.Config) (director commands.DirectorConfiguration) {
 	return
 }
 
-func resources() commands.ResourceConfiguration {
+func resources(envConfig *config.EnvConfig) commands.ResourceConfiguration {
 	f := false
+
 	return commands.ResourceConfiguration{
 		DirectorResourceConfiguration: commands.DirectorResourceConfiguration{
 			InternetConnected: &f,

src/omg-cli/omg/tiles/service_broker/configuration.go
@@ -79,7 +79,7 @@ type SpannerPlan struct {
 	NodeCount   string `json:"num_nodes"`
 }
 
-func (*Tile) Configure(cfg *config.Config, om *ops_manager.Sdk) error {
+func (*Tile) Configure(envConfig *config.EnvConfig, cfg *config.Config, om *ops_manager.Sdk) error {
 	if err := om.StageProduct(tile.Product); err != nil {
 		return err
 	}

src/omg-cli/omg/tiles/stackdriver_nozzle/configuration.go
@@ -39,7 +39,7 @@ type Resources struct {
 	StackdriverNozzle tiles.Resource `json:"stackdriver-nozzle"`
 }
 
-func (t *Tile) Configure(cfg *config.Config, om *ops_manager.Sdk) error {
+func (t *Tile) Configure(envConfig *config.EnvConfig, cfg *config.Config, om *ops_manager.Sdk) error {
 	if err := om.StageProduct(tile.Product); err != nil {
 		return err
 	}
@@ -63,9 +63,17 @@ func (t *Tile) Configure(cfg *config.Config, om *ops_manager.Sdk) error {
 		return err
 	}
 
+	vmType := ""
+	vmCount := 2
+	if envConfig.SmallFootprint {
+		vmType = "micro"
+		vmCount = 1
+	}
 	resoruces := Resources{
 		StackdriverNozzle: tiles.Resource{
 			InternetConnected: false,
+			VmTypeId:          vmType,
+			Instances:         vmCount,
 		},
 	}
 	resorucesBytes, err := json.Marshal(&resoruces)

ci/.gitignore
@@ -1,2 +1,2 @@
 creds/*
-credentials.yml
+credentials*yml

ci/pipeline.yml
@@ -38,6 +38,7 @@ jobs:
 
   - name: generate-env
     serial: true
+    serial_groups: [gcp-project]
     plan:
       - aggregate:
         - {trigger: true, passed: [run-unit], get: omg-src-in, resource: omg-src-in}
@@ -57,6 +58,7 @@ jobs:
 
   - name: create-infrastructure
     serial: true
+    serial_groups: [gcp-project]
     plan:
       - aggregate:
         - {trigger: true, passed: [generate-env], get: omg-src-in, resource: omg-src-in}
@@ -74,6 +76,7 @@ jobs:
 
   - name: push-tiles
     serial: true
+    serial_groups: [gcp-project]
     plan:
       - aggregate:
         - {trigger: true, passed: [create-infrastructure], get: omg-src-in, resource: omg-src-in}
@@ -93,6 +96,7 @@ jobs:
 
   - name: deploy-pcf
     serial: true
+    serial_groups: [gcp-project]
     plan:
       - aggregate:
         - {trigger: true, passed: [push-tiles], get: omg-src-in, resource: omg-src-in}
@@ -115,6 +119,7 @@ jobs:
 
   - name: run-certification
     serial: true
+    serial_groups: [gcp-project]
     plan:
       - aggregate:
         - {trigger: true, passed: [deploy-pcf], get: omg-src-in, resource: omg-src-in}
@@ -131,6 +136,7 @@ jobs:
 
   - name: destroy-pcf
     serial: true
+    serial_groups: [gcp-project]
     plan:
       - aggregate:
         - {trigger: true, passed: [run-certification], get: omg-src-in, resource: omg-src-in}
@@ -141,6 +147,7 @@ jobs:
 
   - name: destroy-infrastructure
     serial: true
+    serial_groups: [gcp-project]
     plan:
       - aggregate:
         - {trigger: true, passed: [destroy-pcf], get: omg-src-in, resource: omg-src-in}

deploy_pcf.sh
@@ -74,7 +74,7 @@ pushd src/omg-tf
             omg-cli review-eulas --env-dir="${ENV_DIR}" --accept-all
         fi
 
-         omg-cli prepare-project --env-dir="${ENV_DIR}"
+        omg-cli prepare-project --env-dir="${ENV_DIR}"
         ./init.sh
     fi
 

commit 4564db3bb7d6d4d7d727c284c092e0d62faf3fc8

tiles/ert: deploy TCP routers in HA (3)
regressed from 03f92b336f1

Change-Id: Ia8503fd3208e10020845072601919908e757d203

src/omg-cli/omg/tiles/ert/configuration.go
@@ -213,6 +213,7 @@ func (*Tile) Configure(envConfig *config.EnvConfig, cfg *config.Config, om *ops_
 
 	zero := 0
 	one := 1
+	three := 3
 	if envConfig.SmallFootprint {
 		resoruces := SmallFootprintResources{
 			TcpRouter: tiles.Resource{
@@ -237,7 +238,7 @@ func (*Tile) Configure(envConfig *config.EnvConfig, cfg *config.Config, om *ops_
 			TcpRouter: tiles.Resource{
 				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.TcpTargetPoolName)},
 				InternetConnected: false,
-				Instances:         &one,
+				Instances:         &three,
 			},
 			Router: tiles.Resource{
 				RouterNames:       []string{fmt.Sprintf("tcp:%s", cfg.WssTargetPoolName), fmt.Sprintf("http:%s", cfg.HttpBackendServiceName)},