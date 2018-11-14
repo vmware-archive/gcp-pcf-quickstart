c6bfb4c6bb660a64d3e82c17df291a5cf526ba91
Matthew Broomfield <mattysweeps@google.com>
2018-11-14 11:14:32 -0800

    Update PAS and Opsman to v2.3

--- src/omg-cli/config/env_config.go
+++ src/omg-cli/config/env_config.go
@@ -39,7 +39,7 @@ type EnvConfig struct {
 func DefaultEnvConfig() (*EnvConfig, error) {
 	c := &EnvConfig{
 		DnsZoneName:        "pcf-zone",
-		BaseImageURL:       "https://storage.cloud.google.com/ops-manager-us/pcf-gcp-2.1-build.326.tar.gz",
+		BaseImageURL:       "https://storage.cloud.google.com/ops-manager-us/pcf-gcp-2.3-build.194.tar.gz",
 		EnvName:            "pcf",
 		Region:             "us-east1",
 		Zone1:              "us-east1-b",
