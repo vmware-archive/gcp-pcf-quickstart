3dbc8f0b7bea179eb967867f670b217b1961372d
Ruben Koster <rkoster@starkandwayne.com>
2018-11-19 10:13:57 +0100

    Bump gcp-service-broker to 4.1.0

--- src/omg-cli/omg/tiles/service_broker/resource.go
+++ src/omg-cli/omg/tiles/service_broker/resource.go
@@ -23,21 +23,23 @@ import (
 var tile = config.Tile{
 	config.PivnetMetadata{
 		"gcp-service-broker",
-		191048,
-		221334,
-		"38b7eea0437af30901803f2ed147e0a167df54e3f4e43096eea11112505efc35",
+		221375,
+		255029,
+		"beb501dd345b322123d0d608bc4c00dbd8343e066249b87a625e9c3cbc15059e",
 	},
 	config.OpsManagerMetadata{
 		"gcp-service-broker",
-		"4.0.0",
+		"4.1.0",
 		true,
 	},
 	&config.StemcellMetadata{
-		config.PivnetMetadata{"stemcells",
-			183841,
-			214146,
-			"93fe455d2ab111cdb5f6902cf8f8552e406e6665f4b46bba5fca37ac47aa0ecd"},
-		"light-bosh-stemcell-3586.42-google-kvm-ubuntu-trusty-go_agent",
+		config.PivnetMetadata{
+			"stemcells",
+			232700,
+			266558,
+			"5d9a7325c05576b0dffa3dcbb7fd02c78a30c56a465cd0ebf39cbfb52f5ca566",
+		},
+		"light-bosh-stemcell-3586.56-google-kvm-ubuntu-trusty-go_agent",
 	},
 }
 
