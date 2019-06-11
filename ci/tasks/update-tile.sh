ef489d342279e3caa3cdf319e6a124625421c1b7
ramonskie <ramonmakkelie@gmail.com>
2019-06-11 17:06:27 +0200

    remove some spaces so it wont intreperted as a command

--- ci/tasks/update-tile.sh
+++ ci/tasks/update-tile.sh
@@ -6,7 +6,7 @@ set -x
 export RECIPE=$(pwd)/repo-in/src/omg-cli/templates/assets/deployment.yml
 
 if [[ ! -z ${OPS_FILE} ]]; then
-  opsfile = "-o ${OPS_FILE}"
+  opsfile="-o ${OPS_FILE}"
 fi
 
 get_tile_var () {
