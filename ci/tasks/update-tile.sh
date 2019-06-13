02b1915f9f43f5c57a640eac743f9e3bc0cd941f
ramonskie <ramonmakkelie@gmail.com>
2019-06-13 16:35:33 +0200

    ability to upgrade opsfiles

--- ci/tasks/update-tile.sh
+++ ci/tasks/update-tile.sh
@@ -3,14 +3,18 @@
 
 set -x
 
-export RECIPE=$(pwd)/repo-in/src/omg-cli/templates/assets/deployment.yml
-
 if [[ ! -z ${OPS_FILE} ]]; then
-  opsfile="--ops-file $(pwd)/repo-in/${OPS_FILE}"
+  opsfile="$(pwd)/repo-in/${OPS_FILE}"
+  opsfilecommand="--ops-file ${opsfile}"
+
 fi
 
+export RECIPE="$(pwd)/repo-in/src/omg-cli/templates/assets/deployment.yml ${opsfilecommand}"
+
+
+
 get_tile_var () {
-    bosh int ${RECIPE} ${opsfile} --path /tiles/name=${TILE_NAME}/${1}
+    bosh int ${RECIPE} --path /tiles/name=${TILE_NAME}/${1}
 }
 
 export PIVNET_PRODUCT_SLUG=$(get_tile_var product/product_slug)
@@ -30,22 +34,37 @@ tile-config-generator generate \
 
 
 echo "Update stemcell to: ${STEMCELL_VERSION} for: ${TILE_NAME}/${PIVNET_PRODUCT_VERSION}"
-bosh int ${RECIPE} -o <(echo -e "
-- type: replace
-  path: /tiles/name=${TILE_NAME}/version
-  value: ${PIVNET_PRODUCT_VERSION}
-- type: replace
-  path: /tiles/name=${TILE_NAME}/product/release_version
-  value: ${PIVNET_PRODUCT_VERSION}
-- type: replace
-  path: /tiles/name=${TILE_NAME}/stemcell/release_version
-  value: ${STMECELL_VERSION}
-") > ${OPS_FILE:-src/omg-cli/templates/assets/deployment.yml}
+if [[ ! -z ${OPS_FILE} ]]; then
+  # ~1 is a special charater for / https://bosh.io/docs/cli-ops-files/#escaping
+  bosh int ${opsfile} -o <(echo -e "
+  - type: replace
+    path: /path=~1tiles~1name=${TILE_NAME}/value/version
+    value: ${PIVNET_PRODUCT_VERSION}
+  - type: replace
+    path: /path=~1tiles~1name=${TILE_NAME}/value/product/release_version
+    value: ${PIVNET_PRODUCT_VERSION}
+  - type: replace
+    path: /path=~1tiles~1name=${TILE_NAME}/value/stemcell/release_version
+    value: ${STMECELL_VERSION}
+  ") > ${OPS_FILE}
+else
+  bosh int ${RECIPE} -o <(echo -e "
+  - type: replace
+    path: /tiles/name=${TILE_NAME}/version
+    value: ${PIVNET_PRODUCT_VERSION}
+  - type: replace
+    path: /tiles/name=${TILE_NAME}/product/release_version
+    value: ${PIVNET_PRODUCT_VERSION}
+  - type: replace
+    path: /tiles/name=${TILE_NAME}/stemcell/release_version
+    value: ${STMECELL_VERSION}
+  ") > src/omg-cli/templates/assets/deployment.yml
+fi
 
 git --no-pager diff
 
 echo "Embed updated template files"
-go generate ./...
+go generate src/omg-cli/templates/templates.go
 
 git config --global user.email "ci@starkandwayne.com"
 git config --global user.name "CI Bot"
