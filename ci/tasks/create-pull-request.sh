0814c6979f5245469957ffc29de005ca164db4bd
ramonskie <ramonmakkelie@gmail.com>
2019-06-20 20:44:25 +0200

    set user-password for pullrequest

--- ci/tasks/create-pull-request.sh
+++ ci/tasks/create-pull-request.sh
@@ -2,6 +2,10 @@
 
 set -x
 
+export GITHUB_USER=${GITHUB_USER}
+export GITHUB_PASSWORD=${GITHUB_PASSWORD}
+
+
 pushd omg-src-in
 
 hub pull-request --base cf-platform-eng/gcp-pcf-quickstart:master -m "Automated Pull Request from the starkandwayne CI"
