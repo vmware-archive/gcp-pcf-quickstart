de5e00cab86405827e52c4320f01104e76d68610
ramonskie <ramonmakkelie@gmail.com>
2019-06-20 20:48:54 +0200

    set head origin for PR

--- ci/tasks/create-pull-request.sh
+++ ci/tasks/create-pull-request.sh
@@ -8,6 +8,6 @@ export GITHUB_PASSWORD=${GITHUB_PASSWORD}
 
 pushd omg-src-in
 
-hub pull-request --base cf-platform-eng/gcp-pcf-quickstart:master -m "Automated Pull Request from the starkandwayne CI"
+hub pull-request --base cf-platform-eng/gcp-pcf-quickstart:master --head starkandwayne/gcp-pcf-quickstart:develop -m "Automated Pull Request from the starkandwayne CI"
 
 popd
