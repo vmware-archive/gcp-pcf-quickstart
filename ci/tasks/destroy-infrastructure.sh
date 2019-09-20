eadfa47b2f6080d119ba59e9e4094cf55d2b89d1
ramonskie <ramonmakkelie@gmail.com>
2019-09-20 15:40:03 +0200

    fix exit on fail

--- ci/tasks/destroy-infrastructure.sh
+++ ci/tasks/destroy-infrastructure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 pushd omg-src-in/ci/tasks > /dev/null
 	source utils.sh
@@ -15,12 +15,15 @@ pushd "${release_dir}/src/omg-tf"
         configure_terraform_backend
 	terraform init
 	terraform get
-	terraform destroy --auto-approve --parallelism=100 -var-file=${terraform_config} && exit 0
-
-	seconds=300
-	echo "terraform destroy failed, trying again in ${seconds} seconds"
-	sleep ${seconds}
 	terraform destroy --auto-approve --parallelism=100 -var-file=${terraform_config}
+	rc=$?
+
+	if [[ ${rc} -ne 0 ]]; then
+		seconds=300
+		echo "terraform destroy failed, trying again in ${seconds} seconds"
+		sleep ${seconds}
+		terraform destroy --auto-approve --parallelism=100 -var-file=${terraform_config}
+  fi
 popd
 
 #quick and dirty cleanup fix
@@ -28,3 +31,5 @@ echo "removing leftover disks"
 gcloud compute disks list --format="value(NAME)" | xargs -L 1 gcloud compute disks delete --zone europe-west4-c -q
 echo "removing leftover stemcells"
 gcloud compute images list --format="value(NAME)" --filter=stemcell | xargs -L 1 gcloud compute images delete -q
+
+exit 0
