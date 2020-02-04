1625e72277214547c4cbf1b5212c8d35067e2c42
ramonskie <ramonmakkelie@gmail.com>
2020-02-04 21:37:39 +0100

    naming issues

--- ci/tasks/shipit.sh
+++ ci/tasks/shipit.sh
@@ -3,14 +3,13 @@
 set -e
 set -x
 
+VERSION=$(cat version/version)
+
 echo "v${VERSION}"           > release/tag
 echo "GCP v${VERSION}"       > release/name
 
-export IMAGE_VERSION=$(cat omg-src-in/src/omg-cli/templates/assets/opsman-image | cut -d- -f5)
+export IMAGE_VERSION=$(cat omg-src-develop/src/omg-cli/templates/assets/opsman-image | cut -d- -f5)
 
-spruce json omg-src-in/src/omg-cli/templates/assets/deployment.yml \
+spruce json omg-src-develop/src/omg-cli/templates/assets/deployment.yml \
     | jq -r '.tiles | map("- \(.name)/\(.version) (\(.stemcell.product_slug | split("-") | .[-1])/\(.stemcell.release_version))") | .[]' > release/notes.md
 echo "- ops-manager/${IMAGE_VERSION}" >> release/notes.md
-
-
-pushd omg-src-develop
